cb5ed08ff26be9a4010f3f6553a499bfc4e766b5
Yemezzy <ayubayelmee222@gmail.com>
2024-01-30 18:38:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Chinese from './Dextool/Chinese';
 import Detials from './Dextool/Detials';
 import Listing from './Dextool/Listing';
 import Update from './Dextool/Update';
@@ -8,7 +9,7 @@ import Update from './Dextool/Update';
 function App() {
   return (
     <div>
-      
+      <Chinese/>
     </div>
 //     <div className=" text-white h-screen">
 //       <Listing />
